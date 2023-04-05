9330b2716a995b5c1b79dbba8ead791d77c0ef6b
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-04-06 01:29:27 +0900

    クラスとインスタンスを使って実装

--- accordion.js
+++ accordion.js
@@ -1,25 +1,50 @@
 (() => {
-    const $elm = document.querySelector('#js-accordion');
-    const $trigger = $elm.getElementsByTagName('a');
 
-    const triggerLen = $trigger.length;
-    let index = 0;
-    while(index < triggerLen) {
-        $trigger[index].addEventListener('click', (e) => clickHandler(e));
-        index++;
+    class Accordion {
+        // 初期化
+        constructor(obj){
+
+            const $elm = document.querySelector(obj.hookName);
+            const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+            const triggerLen = $trigger.length;
+            let index = 0;
+            while(index < triggerLen) {
+                $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+                index++;
+            }
+        }
+
+        // 実行される処理を書く
+        clickHandler = (e) => {
+            e.preventDefault();
+
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
+
+            if($content.style.display === 'block') {
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        };
     }
 
-    // 実行される処理を書く
-    const clickHandler = (e) => {
-        e.preventDefault();
+    // インスタンスの生成
+    const fuckingAccordion = new Accordion({
+        hookName: '#js-faq',
+        tagName: 'p'
+    });
 
-        const $target = e.currentTarget;
-        const $content = $target.nextElementSibling;
+    const dummyAccordion = new Accordion({
+        hookName: '#js-accordion',
+        tagName: 'a'
+    });
 
-        if($content.style.display === 'block') {
-            $content.style.display = 'none';
-        } else {
-            $content.style.display = 'block';
-        }
-    };
+    const miniAccordion = new Accordion({
+        hookName: '#js-accordion-mini',
+        tagName: 'dt'
+    });
 })();
+
+// クラスのコードならインスタンスの値を変えるだけでパターンを量産できる
